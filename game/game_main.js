e4446c8db782c6bd11e751cb6b0256239e5e8281
Rorik <59865515+aurorast1405@users.noreply.github.com>
2021-10-18 18:29:00 +1000

    Update game_main.js

--- game/game_main.js
+++ game/game_main.js
@@ -2,19 +2,41 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
 var y = canvas.height - 60;
+var w = canvas.width
 var speed_x = 1.5;
-var speed_y = -20;
+var speed_y = -60;
+var game_active = false
+var lives = 3
+var distract = [[w / 2 + 400], [w / 2 - 400, w / 2 - 400], [w / 2 + 400], [w / 2 - 400]]
 
 
+document.addEventListener("keydown", keyDownHandler, false);
+//document.addEventListener("keyup", keyUpHandler, false);
 
-// function drawDistraction() {
-//     ctx.beginPath();
-//     ctx.rect(canvas.width / 2 + 100, canvas.height - 60, 50, 50);
-//     ctx.fillStyle = "#FF0000";
-//     ctx.fill();
-//     ctx.closePath();
-//
-// }
+function keyDownHandler(e) {
+    if(e.key === "Up" || e.key === "ArrowUp") {
+        upPressed = true;
+        distract.shift();
+    }
+
+}
+
+function startGame(){
+    if (game_active) game_active = false
+    else game_active = true
+}
+
+function draw_distract(){
+    for (let i = 0; i < distract.length; i++){
+        for (let j of distract[i]) {
+            ctx.beginPath();
+            ctx.rect(j, y + speed_y * i, 50, 50);
+            ctx.fillStyle = "#002aff";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 function drawObject() {
     ctx.beginPath();
@@ -28,12 +50,26 @@ function drawObject() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawObject();
-    // drawDistraction();
-    x += speed_x;
-    if (x >= canvas.width - 40 || x <= 0){
+    draw_distract();
+    if (game_active) x += speed_x;
+    if (distract[0].includes(x - 40) || distract[0].includes(x + 40)){
         speed_x *= -1
-        console.log(speed_x)
+        console.log(distract[0] - 40)
     }
+    else if (x >= canvas.width / 2 + 500 || x <= canvas.width / 2 - 500){
+        lives --
+        game_active = false
+        restart();
+    }
+
+}
+function restart(){
+    x = canvas.width / 2;
+    y = canvas.height - 60;
+    speed_x = 1.5;
+    speed_y = -60;
+    game_active = false
 }
 
-setInterval(draw, 10);
+setInterval(draw, 10)
+//requestAnimationFrame(draw);
